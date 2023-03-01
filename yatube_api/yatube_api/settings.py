96b33f635ee647f058b9c6b07c4fdd4fa551533b
mikhail <pipom3pro@gmail.com>
2023-03-01 20:58:46 +0300

    first

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -24,6 +24,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts.apps.PostsConfig',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -68,6 +71,15 @@ DATABASES = {
 
 
 # Password validation
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
